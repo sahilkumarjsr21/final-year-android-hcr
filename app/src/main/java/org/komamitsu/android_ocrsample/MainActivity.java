256a29bd4e4b55bd2d0642baa5fb5352e8dd832c
SK078475 <SK078475@cerner.net>
2020-06-15 23:14:22 +0530

    Initial Commit

--- app/src/main/java/org/komamitsu/android_ocrsample/MainActivity.java
+++ app/src/main/java/org/komamitsu/android_ocrsample/MainActivity.java
@@ -1,15 +1,24 @@
 package org.komamitsu.android_ocrsample;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
 import android.app.AlertDialog;
 import android.content.ContentValues;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.media.ExifInterface;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
+import android.speech.tts.TextToSpeech;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
@@ -19,19 +28,38 @@ import android.util.DisplayMetrics;
 import android.util.Log;
 import android.util.SparseArray;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.google.android.gms.vision.Frame;
 import com.google.android.gms.vision.text.TextBlock;
 import com.google.android.gms.vision.text.TextRecognizer;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.ObjectOutputStream;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Locale;
+
+import okhttp3.FormBody;
+import okhttp3.MediaType;
+import okhttp3.MultipartBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
     private static final int REQUEST_GALLERY = 0;
@@ -41,8 +69,11 @@ public class MainActivity extends AppCompatActivity {
     private static final String TAG = MainActivity.class.getSimpleName();
 
     private Uri imageUri;
-    private TextView detectedTextView;
-
+    private TextView detectedTextView, emptyTextView;
+    private TextToSpeech textToSpeech;
+    private Button speakButton;
+    private String currentPhotoPath;
+    private boolean connected = false;
     @Override
     public void onRequestPermissionsResult(int requestCode,
                                            @NonNull String permissions[], @NonNull int[] grantResults) {
@@ -61,8 +92,7 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    private void requestPermissions()
-    {
+    private void requestPermissions() {
         List<String> requiredPermissions = new ArrayList<>();
 
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
@@ -88,6 +118,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         requestPermissions();
+        speakButton = findViewById(R.id.speachButton);
+        emptyTextView = findViewById(R.id.emptyText);
+
 
         findViewById(R.id.choose_from_gallery).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -103,12 +136,10 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 String filename = System.currentTimeMillis() + ".jpg";
-
                 ContentValues values = new ContentValues();
                 values.put(MediaStore.Images.Media.TITLE, filename);
                 values.put(MediaStore.Images.Media.MIME_TYPE, "image/jpeg");
                 imageUri = getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
-
                 Intent intent = new Intent();
                 intent.setAction(MediaStore.ACTION_IMAGE_CAPTURE);
                 intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
@@ -118,9 +149,79 @@ public class MainActivity extends AppCompatActivity {
 
         detectedTextView = (TextView) findViewById(R.id.detected_text);
         detectedTextView.setMovementMethod(new ScrollingMovementMethod());
+
+        textToSpeech = new TextToSpeech(this, new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int i) {
+                if (i == TextToSpeech.SUCCESS) {
+                    int result = textToSpeech.setLanguage(Locale.ENGLISH);
+                    if (result == TextToSpeech.LANG_MISSING_DATA
+                            || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                        Log.e("TTS", "Language not supported");
+                    } else {
+                        speakButton.setEnabled(true);
+                    }
+                } else {
+                    Log.e("TTS", "Initialization failed");
+                }
+            }
+        });
+
+        speakButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textToSpeech.speak(detectedTextView.getText().toString(), TextToSpeech.QUEUE_FLUSH, null);
+            }
+        });
     }
 
-    private void inspectFromBitmap(Bitmap bitmap) {
+    private void inspectFromBitmap(Bitmap bitmap) throws IOException {
+        ConnectivityManager connectivityManager = (ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE);
+        if(connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_MOBILE).getState() == NetworkInfo.State.CONNECTED ||
+                connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI).getState() == NetworkInfo.State.CONNECTED) {
+            //we are connected to a network
+            connected = true;
+        }
+        else
+            connected = false;
+
+        System.out.println("*!#)!*#)!(#*!)(#*!)#(*!)#*!"+connected);
+        if(connected==true) {
+            File imageFile = new File(Environment.getExternalStorageDirectory() + "/image.jpg");
+            imageFile.createNewFile();
+            if (!imageFile.exists()) {
+                System.out.println("MainActivity.inspectFromBitmap" + " make dirs");
+                imageFile.mkdir();
+            }
+            FileOutputStream fout = new FileOutputStream(imageFile);
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, fout);
+            fout.flush();
+            fout.close();
+            int rotate = 0;
+
+            ExifInterface exifInterface = new ExifInterface(imageFile.getAbsolutePath());
+            int orientation = exifInterface.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_NORMAL);
+
+            switch (orientation) {
+                case ExifInterface.ORIENTATION_ROTATE_270:
+                    rotate = 270;
+                    break;
+                case ExifInterface.ORIENTATION_ROTATE_180:
+                    rotate = 180;
+                    break;
+                case ExifInterface.ORIENTATION_ROTATE_90:
+                    rotate = 90;
+                    break;
+            }
+            Matrix matrix = new Matrix();
+            Bitmap adjustedBitmap = null;
+            if (orientation != 0) {
+                matrix.preRotate(rotate);
+                adjustedBitmap = Bitmap.createBitmap(bitmap, 0, 0, bitmap.getWidth(), bitmap.getHeight(), matrix, true);
+            }
+            OKhttpHandler handler = new OKhttpHandler();
+            handler.execute(imageFile);
+        }else {
         TextRecognizer textRecognizer = new TextRecognizer.Builder(this).build();
         try {
             if (!textRecognizer.isOperational()) {
@@ -156,15 +257,25 @@ public class MainActivity extends AppCompatActivity {
                     detectedText.append("\n");
                 }
             }
-
-            detectedTextView.setText(detectedText);
-        }
-        finally {
+            System.out.println(detectedText.toString());
+            if (!detectedText.toString().isEmpty()) {
+                detectedTextView.setText(detectedText.toString());
+                detectedTextView.setVisibility(View.VISIBLE);
+                emptyTextView.setVisibility(View.GONE);
+                speakButton.setVisibility(View.VISIBLE);
+            } else {
+                emptyTextView.setVisibility(View.VISIBLE);
+                speakButton.setVisibility(View.GONE);
+                speakButton.setEnabled(false);
+            }
+        } finally {
             textRecognizer.release();
         }
+        }
+//
     }
 
-    private void inspect(Uri uri) {
+    private void inspect(Uri uri) throws IOException {
         InputStream is = null;
         Bitmap bitmap = null;
         try {
@@ -196,19 +307,115 @@ public class MainActivity extends AppCompatActivity {
         switch (requestCode) {
             case REQUEST_GALLERY:
                 if (resultCode == RESULT_OK) {
-                    inspect(data.getData());
+                    try {
+                        inspect(data.getData());
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
                 }
                 break;
             case REQUEST_CAMERA:
                 if (resultCode == RESULT_OK) {
                     if (imageUri != null) {
-                        inspect(imageUri);
+                        try {
+                            inspect(imageUri);
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
                     }
                 }
                 break;
             default:
                 super.onActivityResult(requestCode, resultCode, data);
                 break;
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        if (textToSpeech != null) {
+            textToSpeech.stop();
+            textToSpeech.shutdown();
+        }
+        super.onDestroy();
+    }
+
+    public class OKhttpHandler extends AsyncTask<File,Void,String> {
+
+        OkHttpClient client = new OkHttpClient();
+
+        @SuppressLint("WrongThread")
+        @Override
+        protected String doInBackground(File[] objects) {
+            File object = objects[0];
+            ByteArrayOutputStream bos = new ByteArrayOutputStream();
+            ObjectOutputStream oos = null;
+            try {
+                oos = new ObjectOutputStream(bos);
+                oos.writeObject(object);
+                oos.flush();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            byte[] data = bos.toByteArray();
+            FileInputStream fileInputStreamReader = null;
+            try {
+                fileInputStreamReader = new FileInputStream(objects[0]);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+            byte[] bytes = new byte[(int)objects[0].length()];
+            try {
+                fileInputStreamReader.read(bytes);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            System.out.println(objects[0].getAbsolutePath());
+            RequestBody requestBody = new MultipartBody.Builder()
+                    .setType(MultipartBody.FORM)
+                    .addFormDataPart("srcUrl","https://pen-to-print-handwriting-ocr.p.rapidapi.com/recognize/")
+                    .addFormDataPart("srcImg","/tmp/",RequestBody.create(object,MediaType.parse("image/jpg")))
+                    .addFormDataPart("includeSubScan",Integer.toString(0))
+                    .addFormDataPart("session","GUID")
+                    .build();
+            Request request = new Request.Builder()
+                    .url("https://pen-to-print-handwriting-ocr.p.rapidapi.com/recognize/")
+                    .post(requestBody)
+                    .addHeader("x-rapidapi-host", "pen-to-print-handwriting-ocr.p.rapidapi.com")
+                    .addHeader("x-rapidapi-key", "dc465a5656mshbf6daac7d42a52ep188568jsn214b8bb97023")
+                    .addHeader("content-type", "multipart/form-data")
+                    .build();
+            System.out.println(request.toString());
+            System.out.println(request.body().contentType());
+            Response response = null;
+            try {
+                response = client.newCall(request).execute();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            System.out.println("!#^!*&@^#*!@^#*!^#!*^#!*#^*#" + response.code());
+            String x= "";
+
+            if (response.isSuccessful()) {
+                try {
+                    JSONObject jsonObject = new JSONObject(response.body().string());
+                    x = jsonObject.getString("value");
+                } catch (IOException | JSONException e) {
+                    e.printStackTrace();
+                }
+
+                return x;
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String o) {
+            super.onPostExecute(o);
+            System.err.println("OKhttpHandler.onPostExecute"+o);
+            detectedTextView.setText(o);
+            detectedTextView.setVisibility(View.VISIBLE);
+            emptyTextView.setVisibility(View.GONE);
         }
     }
 }
